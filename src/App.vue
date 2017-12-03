e7c06d36abd32a49e735225b9b25b97c8a0fa40c
Bajal <bajal123@gmail.com>
2017-12-02 19:25:06 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.row {
+    background-color: aliceblue;
+    /* border-radius: 15px; 
+    box-shadow: 0px 0px 6px #888;*/
+    border-color: black;
+}
+.col {
+    background-color: aliceblue;
+    /* border-radius: 15px;
+    box-shadow: 0px 0px 6px #888;*/
+    border-color: black;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <b-alert show>{{ msg }}</b-alert>
+    <!-- <b-container fluid>
+    
+    </b-container> -->
+    <b-container class="bv-example-row">
+    <b-row>
+      
+        <b-col>
+          <label for="input-small">V4Address:</label>
+          <b-form-input v-model="text1"
+                  type="text"
+                  placeholder="Enter your name"></b-form-input>
+        </b-col>
+        <b-col>2 of 2</b-col>
+    </b-row>
+    <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+    </b-row>
+</b-container>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to SDN-GP'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
